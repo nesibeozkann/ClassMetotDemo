3caf86f3c19c0559f6823c5ddb77c023b5d3c17e
nesibeozkann <77546356+nesibeozkann@users.noreply.github.com>
2021-01-25 23:23:08 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        List<Musteri> musteriler = new List<Musteri>();
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("musteri eklendi:" + musteri.Id + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.TelefonNo);
+        }
+        public void List()
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.TelefonNo);
+
+            }
+
+        }
+        public void Delete(Musteri musteri)
+        {
+            musteriler.Remove(musteri);
+            Console.WriteLine("\n -" + musteri.Ad + " silindi.");
+        }
+        
+
+        }
+
+    }
+
+    
+
+
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Ad = "Nesibe";
+            string Soyad = "Özkan";
+            int TelefonNo = 123456;
+
+            Musteri musteri1= new Musteri(); //classın örneği
+            musteri1.Id = 1;
+            musteri1.Ad = "Nesibe";
+            musteri1.Soyad = "Özkan";
+            musteri1.TelefonNo = 12345;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Fatma";
+            musteri2.Soyad = "ÖCAL";
+            musteri2.TelefonNo = 23564;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Tuba";
+            musteri3.Soyad = "Genli";
+            musteri3.TelefonNo = 66345;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2,musteri3 };
+          
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Ad);
+                Console.WriteLine(musteri.Soyad);
+                Console.WriteLine(musteri.TelefonNo);
+                Console.WriteLine("--------------------------------");
+
+            }
+        
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.List();
+            musteriManager.Delete(musteri2);
+            
+            musteriManager.List();
+            
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
